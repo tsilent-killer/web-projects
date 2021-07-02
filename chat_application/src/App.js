e0821ee049e315704b5d96b3a00340b2b091b9a6
tsilent_killer <gamingboyaditya@gmail.com>
2021-07-02 22:44:14 +0530

    adding login functionality

--- chat_application/src/App.js
+++ chat_application/src/App.js
@@ -1,15 +1,22 @@
 import { ChatEngine } from 'react-chat-engine';
+
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '0fdfe580-8f5c-4b9e-ad50-332ef8bd535a';
+
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
-			height='100vh'
-			userName='Aditya'
-			userSecret='aditya'
-			projectID='0fdfe580-8f5c-4b9e-ad50-332ef8bd535a'
-		/>
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
 
 export default App;
